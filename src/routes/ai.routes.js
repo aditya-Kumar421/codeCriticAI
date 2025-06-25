9aa19887bb819ef84ff18174a4f5358630091e7d
Aditya <ak.adityakumar421@gmail.com>
2025-06-25 22:44:39 +0530

    first commit

---
+++ src/routes/ai.routes.js
@@ -0,0 +1,9 @@
+const express  = require('express');
+
+const aiController = require('../controllers/ai.controller');
+
+const router = express.Router();
+
+router.post('/get-response', aiController.getResponse)
+
+module.exports = router;
